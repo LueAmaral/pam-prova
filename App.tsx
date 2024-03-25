6cdf6cf69e727d84bd4f037c4e82e3cada63d7fb
LueAmaral <lue.amaral@etec.sp.gov.br>
2024-03-25 19:44:36 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [base, setBase] = React.useState('');
+  const [altura, setAltura] = React.useState('');
+  const areaTriangulo = (parseFloat(base) * parseFloat(altura)) / 2;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.titulop}>Calcule a área do triângulo</Text>
+
+      <Text style={styles.titulo}>Base:</Text>
+      <TextInput onChangeText={setBase} keyboardType='numeric' style={styles.textinp}/>
+
+      <Text style={styles.titulo}>Altura:</Text>
+      <TextInput onChangeText={setAltura} keyboardType='numeric' style={styles.textinp}/>
+
+      <Text style={styles.titulo}>Área do triangulo:</Text>
+      {!isNaN(areaTriangulo) && <Text style={styles.resultado}>{areaTriangulo}</Text>}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,48 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a9a9a9',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 10,
+
+    },
+  titulop: {
+    fontSize: 25,
+    marginBottom: 50,
+    fontWeight: 'bold',
+    color: 'white',
   },
+  titulo: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'flex-start',
+  },
+  resultado: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    margin: 25,
+  },
+  textinp: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#696969',
+    borderRadius: 12,
+    color: '#ffffff',
+    margin: 10,
+    padding: 15,
+    marginBottom: 30,
+  },
+  botao: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    borderRadius: 100,
+  },
+  btntxt: {
+    alignSelf: 'center',
+    color: 'white',
+  }
 });
